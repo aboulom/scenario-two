efe951e1d4def3e28865b18365c36064ac81198a
aboulom <allen@opensourcearchitect.co>
2016-07-15 14:10:25 -0500

    Scenario-Two
    This project demonstrates a scenario where a trip file must be
    processed before a traveler file

---
+++ bundle/src/main/java/HashmapProcessor.java
@@ -0,0 +1,21 @@
+package com.opensourcearchitect;
+
+import java.util.*;
+
+public class HashmapProcessor {
+
+	// Create a hash map
+    HashMap hm = new HashMap();
+
+	public void store (String tripID, String comment) {
+      // Put elements to the map
+      hm.put(tripID, comment);
+	}
+	
+	public String check (String ID, String tripID) {
+		if (hm.get(tripID) == null)
+			return "Trip ID [" + tripID + "] not found for Traveler ID [" + ID + "]";
+		else
+			return "Trip ID [" + tripID + "] found for Traveler ID [" + ID + "]";
+	}
+}
